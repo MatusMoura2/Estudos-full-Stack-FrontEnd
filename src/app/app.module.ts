65028f2870cd2508736358a5b95990623f2fdabe
Matus Moura <matus.moura333x2@gmail.com>
2024-02-12 09:16:01 -0300

    FormsModule e HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from'@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { MainComponent } from './main/main.component';
     MainComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
